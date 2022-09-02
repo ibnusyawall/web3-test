8b0cef35ac5f337488d4d564b563171e04d4f4e5
ibnusyawall <website.isas.24@gmail.com>
2022-09-03 05:16:04 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Screenshot
+<p align="center">
+    <img alt="screenshot show wallet and balance" src="https://raw.githubusercontent.com/ibnusyawall/web3-test/main/src/images/ss1.jpg"/>
+</p>
+
+## Contact Me
+
+<a href="https://wa.me/6282299265151"><img alt="WhatsApp" src="https://img.shields.io/badge/WhatsApp%20-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"/><a>
---
+++ src/scripts/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    var connect = $('#connect');
+    var connected_wallet = $('#connected_wallet');
+    var connected_balance = $('#connected_balance');
+
+    connected_wallet.hide();
+    connected_balance.hide();
+
+    if (!window.ethereum) {
+        alert('Browser not supported.');
+    }
+
+    if (typeof window.ethereum !== "undefined") {
+        var web3 = web3 = new Web3(window.ethereum);
+
+        web3.eth.getAccounts()
+            .then(data => {
+                web3.eth.getBalance(data[0])
+                    .then(balance => {
+                        connected_balance.show();
+                        connected_balance.text(`Balance: ${balance}`);
+                    })
+                    .catch(e => alert(e))
+            })
+            .catch(e => alert(e))
+    }
+
+    connect.on('click', function(ev) {
+        ethereum
+           .request({ method: "eth_requestAccounts" })
+           .then((accounts) => {
+               let account = accounts[0]
+               connected_wallet.show();
+               connected_wallet.text(`Wallet [ETH]: ${account}`);
+           })
+           .catch(e => {
+               alert(`Fail connect to wallet address:( | code: ${e.code}`);
+           })
+    });
+});
